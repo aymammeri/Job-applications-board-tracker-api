691bbee019e598f33f693edfe0856983ee413574
aymammeri <ay.mammeri@gmail.com>
2022-02-14 00:42:39 -0500

    Change Database name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'Job-applications-board-tracker-db'
+const mongooseBaseName = 'jobHunt'
 
 // create the mongodb uri for development and test
 const database = {
